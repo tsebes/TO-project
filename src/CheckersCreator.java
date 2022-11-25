58be1870f1613fbf8b510c275d8360ee5ba7186b
wojzam <wojciech.zamarski@student.pk.edu.pl>
2022-11-25 16:18:38 +0100

    add basic components of factory method

---
+++ src/CheckersCreator.java
@@ -0,0 +1,6 @@
+public class CheckersCreator implements GameCreator {
+    @Override
+    public BoardGame crateGame() {
+        return new CheckersGame();
+    }
+}
---
+++ src/CheckersGame.java
@@ -0,0 +1,6 @@
+public class CheckersGame implements BoardGame {
+    @Override
+    public void start() {
+        System.out.println("Checkers game - start");
+    }
+}
