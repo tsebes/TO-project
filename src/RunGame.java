18e8d9a6b7bff083a1cc75690d66a33bde42c9f3
Nikodem Zbaraza <nikozet@interia.pl>
2022-12-16 18:03:39 +0100

    added platform and singleton to run choosen game

---
+++ src/RunGame.java
@@ -0,0 +1,31 @@
+
+//singleton odpowiadajacy za uruchomienie tylko jednej gry w danym momencie
+public class RunGame {
+    private static RunGame instance;
+    private String value;
+
+    private RunGame(String value) {
+        this.value = value;
+        if(value.equals("Checkers")){
+            GameCreator creator = new CheckersCreator();
+            BoardGame game = creator.crateGame();
+            game.start();
+        }
+        else if(value.equals("Knights")){
+            GameCreator creator = new KnightsCreator();
+            BoardGame game = creator.crateGame();
+            game.start();
+        }
+    }
+    //usuwanie instancji
+    public static void clean() {
+        instance = null;
+    }
+
+    public static RunGame getInstance(String value) {
+        if (instance == null) {
+            instance = new RunGame(value);
+        }
+        return instance;
+    }
+}
