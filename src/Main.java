2b45c066f03e905b47a2e8988eeff9afda3a9447
Nikodem Zbaraza <nikozet@interia.pl>
2022-12-16 18:08:09 +0100

    changed main on the branch

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        Board board = new Board();
-        board.show();
+        Platform platform = new Platform();
+        platform.showPlatform();
     }
 }
