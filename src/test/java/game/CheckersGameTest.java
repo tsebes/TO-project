25689f5862e294d22d2bf1d995fa45e54eacb9d5
tsebes <sebastian.zych@student.pk.edu.pl>
2023-01-24 22:47:37 +0100

    added taking tests and changing into king tests

--- src/test/java/game/CheckersGameTest.java
+++ src/test/java/game/CheckersGameTest.java
@@ -2,14 +2,20 @@ package game;
 
 import game.creators.CheckersCreator;
 import game.enums.PieceType;
+import game.enums.Player;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
+import static game.Board.TILE_COUNT;
+import static org.mockito.Mockito.*;
+
 import java.util.HashSet;
 import java.util.Set;
+
+
 
 public class CheckersGameTest {
 
@@ -173,4 +179,129 @@ public class CheckersGameTest {
         Assertions.assertTrue(game.getBoard().getField(start).isEmpty());
         Assertions.assertSame(game.getBoard().getField(end).getPiece(), movedPiece);
     }
+
+    @Test
+    public void shouldTakeWhitePiece() {
+        Field[][] fields = new Field[TILE_COUNT][TILE_COUNT];
+        for (int i = 0; i < TILE_COUNT; i++) {
+            for (int j = 0; j < TILE_COUNT; j++) {
+                fields[i][j] = new Field();
+            }
+        }
+        fields[0][1].setPiece(new Piece(Player.BLACK, PieceType.MAN));
+        fields[3][4].setPiece(new Piece(Player.BLACK, PieceType.MAN));
+        fields[7][0].setPiece(new Piece(Player.WHITE, PieceType.MAN));
+        fields[4][3].setPiece(new Piece(Player.WHITE, PieceType.MAN));
+
+        Board customBoard = new Board(fields);
+
+        CheckersCreator mockCreator = mock(CheckersCreator.class);
+        when(mockCreator.createGame()).thenReturn(new CheckersGame(customBoard));
+        BoardGame startedGame = mockCreator.createGame();
+
+        Coordinates start = new Coordinates(4, 3);
+        Coordinates taken = new Coordinates(3,4);
+        Coordinates end = new Coordinates(2, 5);
+
+        Piece movedPiece = startedGame.getBoard().getField(start).getPiece();
+
+        Assertions.assertTrue(startedGame.getBoard().getField(end).isEmpty());
+
+        startedGame.move(start, end);
+
+        Assertions.assertTrue(startedGame.getBoard().getField(start).isEmpty());
+        Assertions.assertTrue(startedGame.getBoard().getField(taken).isEmpty());
+        Assertions.assertSame(startedGame.getBoard().getField(end).getPiece(), movedPiece);
+    }
+
+    @Test
+    public void shouldTakeBlackPiece() {
+        Field[][] fields = new Field[TILE_COUNT][TILE_COUNT];
+        for (int i = 0; i < TILE_COUNT; i++) {
+            for (int j = 0; j < TILE_COUNT; j++) {
+                fields[i][j] = new Field();
+            }
+        }
+        fields[0][1].setPiece(new Piece(Player.BLACK, PieceType.MAN));
+        fields[2][3].setPiece(new Piece(Player.BLACK, PieceType.MAN));
+        fields[7][0].setPiece(new Piece(Player.WHITE, PieceType.MAN));
+        fields[3][4].setPiece(new Piece(Player.WHITE, PieceType.MAN));
+
+        Board customBoard = new Board(fields);
+
+        CheckersCreator mockCreator = mock(CheckersCreator.class);
+        when(mockCreator.createGame()).thenReturn(new CheckersGame(customBoard));
+        BoardGame startedGame = mockCreator.createGame();
+
+        Coordinates start = new Coordinates(2, 3);
+        Coordinates taken = new Coordinates(3,4);
+        Coordinates end = new Coordinates(4, 5);
+
+        startedGame.currentTurn = Player.BLACK;
+
+        Piece movedPiece = startedGame.getBoard().getField(start).getPiece();
+
+        Assertions.assertTrue(startedGame.getBoard().getField(end).isEmpty());
+
+        startedGame.move(start, end);
+
+        Assertions.assertTrue(startedGame.getBoard().getField(start).isEmpty());
+        Assertions.assertTrue(startedGame.getBoard().getField(taken).isEmpty());
+        Assertions.assertSame(startedGame.getBoard().getField(end).getPiece(), movedPiece);
+    }
+
+    @Test
+    public void shouldWhiteChangeToKing() {
+        Field[][] fields = new Field[TILE_COUNT][TILE_COUNT];
+        for (int i = 0; i < TILE_COUNT; i++) {
+            for (int j = 0; j < TILE_COUNT; j++) {
+                fields[i][j] = new Field();
+            }
+        }
+        fields[0][1].setPiece(new Piece(Player.BLACK, PieceType.MAN));
+        fields[7][0].setPiece(new Piece(Player.WHITE, PieceType.MAN));
+        fields[1][6].setPiece(new Piece(Player.WHITE, PieceType.MAN));
+
+        Board customBoard = new Board(fields);
+
+        CheckersCreator mockCreator = mock(CheckersCreator.class);
+        when(mockCreator.createGame()).thenReturn(new CheckersGame(customBoard));
+        BoardGame startedGame = mockCreator.createGame();
+
+        Coordinates start = new Coordinates(1, 6);
+        Coordinates end = new Coordinates(0, 7);
+
+        startedGame.move(start, end);
+
+        Assertions.assertEquals(startedGame.getBoard().getField(end).getPiece().getPieceType(), PieceType.KING);
+    }
+
+    @Test
+    public void shouldBlackChangeToKing() {
+        Field[][] fields = new Field[TILE_COUNT][TILE_COUNT];
+        for (int i = 0; i < TILE_COUNT; i++) {
+            for (int j = 0; j < TILE_COUNT; j++) {
+                fields[i][j] = new Field();
+            }
+        }
+        fields[0][1].setPiece(new Piece(Player.BLACK, PieceType.MAN));
+        fields[7][0].setPiece(new Piece(Player.WHITE, PieceType.MAN));
+        fields[6][7].setPiece(new Piece(Player.BLACK, PieceType.MAN));
+
+        Board customBoard = new Board(fields);
+
+        CheckersCreator mockCreator = mock(CheckersCreator.class);
+        when(mockCreator.createGame()).thenReturn(new CheckersGame(customBoard));
+        BoardGame startedGame = mockCreator.createGame();
+
+        startedGame.currentTurn = Player.BLACK;
+
+        Coordinates start = new Coordinates(6, 7);
+        Coordinates end = new Coordinates(7, 6);
+
+        startedGame.move(start, end);
+
+        Assertions.assertEquals(startedGame.getBoard().getField(end).getPiece().getPieceType(), PieceType.KING);
+    }
+
 }
